f8ed4bfee48de0e1ff5541163cb360074ee0a349
Emeka Jideije <jideije.emeka@gmail.com>
2022-01-18 21:09:17 +0100

    Add the api store for storing the api required

---
+++ lib/src/api/api_store.dart
@@ -0,0 +1,26 @@
+import 'package:dio/dio.dart';
+
+class ApiStore {
+  final String url;
+  final Map<String, dynamic>? payload;
+
+  ApiStore({required this.url, required this.payload});
+
+  final Dio _dio = Dio();
+
+  void get({
+    Function()? beforeSend,
+    Function(dynamic data)? onSuccess,
+    Function(dynamic onError)? onError,
+  }) {
+    _dio.get(url, queryParameters: payload).then((response) {
+      if (onSuccess != null) {
+        onSuccess(response.data);
+      }
+    }).catchError((e) {
+      if (onError != null) {
+        onError(e);
+      }
+    });
+  }
+}
